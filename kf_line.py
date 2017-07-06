e6c04cb434f14256ba0b2ec0b036fa118762fa80
Takio Yamaoka <y.takio@gmail.com>
2017-07-06 22:32:16 +0900

    temporary completed

--- kf_line.py
+++ kf_line.py
@@ -2,20 +2,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-p_sd = 10
-p_noise = np.random.normal(0, p_sd, size=10000) # 10 m/s^2
-m_sd = 0.1
-m_noise = np.random.normal(0, m_sd, size=10000) # 10 cm
+itr = 1000
+p_sd = 10 # 10[m/s^2]
+p_noise = np.random.normal(0, p_sd, size=itr)
+p_noise[0] = 0.
+m_sd = 0.01 # 10[cm]
+m_noise = np.random.normal(0, m_sd, size=itr)
 
-dt = 0.001   # 1 msec
-x = np.matrix([[0], [0]]) # state: [position, velocity]
-F = np.matrix([[1, dt], [0, 1]])
-G = np.matrix([[dt**2/2], [dt]])
-H = np.matrix([[1, 0]])
+dt = 0.01   # 1 msec
+x = np.zeros( (2,itr) ) # true: position, velocity
+y = np.zeros( (1,itr) ) # true: position, velocity
+x_ = np.zeros( (2,itr) ) # predected: position, velocity
+F = np.array([
+    [1, dt],
+    [0, 1],
+    ])
+G = np.array([
+    [dt**2/2],
+    [dt],
+    ])
+H = np.array([
+    [1, 0],
+    ])
 
-P = np.matrix([[0, 0], [0, 0]])
-Q = p_sd**2 * (G * G.T)
-R = m_sd**2
+P = np.array( [
+    [0, 0],
+    [0, 0],
+    ])
+Q = np.diag([p_sd**2])
+R = np.diag([m_sd**2])
+I = np.eye(2,2)
 
-plt.plot(samples)
+for i in range(1, itr):
+    # preparation
+    x[:,i] = (F @ x[:,i-1].reshape(2,1) + G * p_noise[i]).flatten()
+    xv = x_[:,i-1].reshape(2,1)
+    # predict
+    xp = F @ xv
+    P = F @ P @ F.T + G @ Q @ G.T
+    # observe and update
+    y[:,i] = H @ x[:,i].reshape(2,1) + m_noise[i]
+    e = y[:,i] - H @ xp
+    S = R + H @ P @ H.T
+    K = P @ H.T @ np.linalg.inv(S)
+    x_[:,i] = (xv + K @ e).flatten()
+    P = (I - K @ H) @ P
+
+#plt.plot(y[0])
+plt.plot(x[1])
+plt.plot(x_[1])
 plt.show()
