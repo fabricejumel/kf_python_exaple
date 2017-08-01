8a721318f00637ddcf4ce9292b83b15c3d506c1a
Yamaoka Takio <yt5708@paltac.co.jp>
2017-08-01 15:48:08 +0900

    Update for README.md

--- README.md
+++ README.md
@@ -1,10 +1,49 @@
 # kf_python_exaple
 Python example for Kalman Filter
 
+This is sample python code about [WikiPage](https://en.wikipedia.org/wiki/Kalman_filter#Example_application.2C_technical) page for Kalman Filter setting example.
+
 It's depended on numpy and matplotlib.
 
+## Parameters
+- sampling time (delta): 10 [msec]
+- num of sample (itr): 100
+- num of state (x_num): 2
+- num of observation (z_num): 1
+
+## Interface of function
+```
+def kf(Phi, G, H, P, Q, R, z, x, u=0):
+    """ Kalman Filtering function
+        x(k) = Phi@x(k-1) + u(k-1) + w(k-1)
+        z(k) = R@x(k) + v(k)
+        w(k) := N(0, Q)
+        v(k) := N(0, R)
+    Args:
+        Phi: dynamics
+        G: System noise model
+        H: Observation model
+        P: Current error covariance matrix
+        Q: System noise covariance matrix
+        R: Observation noise covariance matrix
+        z: Observation vector
+        x: State vector
+        u: Control input
+    Returns: xp, xf, newP, K
+        xp: Predicted state
+        xf: Filtered state
+        newP: Updated P
+        K: Kalman gain
+    """
+```
+
+
 # Execute
+## Run
 ```
 $ python kf_wiki_sample.py
 ```
+
+## Result
+![Result](./result.png)
 
--- kf_wiki_sample.py
+++ kf_wiki_sample.py
@@ -3,18 +3,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def kf(Phi, G, H, P, Q, R, z, x, u=0):
+    """ Kalman Filtering function
+        x(k) = Phi@x(k-1) + u(k-1) + w(k-1)
+        z(k) = R@x(k) + v(k)
+        w(k) := N(0, Q)
+        v(k) := N(0, R)
+    Args:
+        Phi: dynamics
+        G: System noise model
+        H: Observation model
+        P: Current error covariance matrix
+        Q: System noise covariance matrix
+        R: Observation noise covariance matrix
+        z: Observation vector
+        x: State vector
+        u: Control input
+    Returns: xp, xf, newP, K
+        xp: Predicted state
+        xf: Filtered state
+        newP: Updated P
+        K: Kalman gain
+    """
     xp = Phi @ x + u;
     Pm = Phi @ P @ Phi.T + G @ Q @ G.T
     S = H @ Pm @ H.T + R
     K = Pm @ H.T @ np.linalg.inv(S)
     xf = xp + K @ (z - H @ xp)
-    Pp = (np.eye(x.size) - K @ H) @ Pm
+    newP = (np.eye(x.size) - K @ H) @ Pm
 
-    return xp, xf, Pp, K
+    return xp, xf, newP, K
 
 itr = 100                         # iteration
 x_num = 2                           # state vector
-y_num = 1                           # observation vector
+z_num = 1                           # observation vector
 
 delta = 0.01
 
@@ -35,14 +56,14 @@ G = np.array([
     ]).reshape(x_num, system_sds.size)
 H = np.array([
     1, 0,
-    ]).reshape(y_num, x_num)
+    ]).reshape(z_num, x_num)
 
 x = np.zeros( (itr, x_num, 1) )     # true state
 xm = np.zeros( x.shape )            # predected state
 xh = np.zeros( x.shape )            # predected state
-y = np.zeros( (itr, y_num, 1) )     # observation array
+z = np.zeros( (itr, z_num, 1) )     # observation array
 P = np.zeros( (itr, x_num, x_num) )
-K = np.zeros( (itr, x_num, y_num) )
+K = np.zeros( (itr, x_num, z_num) )
 
 Q = np.diag(system_sds.flatten()**2)
 R = np.diag(measure_sds.flatten()**2)
@@ -50,10 +71,10 @@ R = np.diag(measure_sds.flatten()**2)
 system_noise = np.random.randn(itr, system_sds.size, 1) * system_sds
 measure_noise = np.random.randn(itr, measure_sds.size, 1) * measure_sds
 
-y[0] = H @ x[0] + measure_noise[0]
+z[0] = H @ x[0] + measure_noise[0]
 for i in range(1, itr):
     x[i] = Phi @ x[i-1] + G @ system_noise[i-1]
-    y[i] = H @ x[i] + measure_noise[i]
+    z[i] = H @ x[i] + measure_noise[i]
     pass
 
 # Initialize
@@ -61,7 +82,7 @@ xh[0] = 0
 P[0] = 0
 K[0] = 0
 for i in range(1, itr):
-    xm[i], xh[i], P[i], K[i] = kf(Phi, G, H, P[i-1], Q, R, y[i], xh[i-1])
+    xm[i], xh[i], P[i], K[i] = kf(Phi, G, H, P[i-1], Q, R, z[i], xh[i-1])
     pass
 
 subplt_num = 4
@@ -69,7 +90,7 @@ subplt_loc = 0;
 
 subplt_loc += 1;
 plt.subplot(subplt_num,1,subplt_loc)
-plt.plot(y[:,0,:].flatten(), label="Observed")
+plt.plot(z[:,0,:].flatten(), label="Observed")
 plt.plot(xm[:,0,:].flatten(), label="Predicted")
 plt.plot(xh[:,0,:].flatten(), label="Filtered")
 plt.plot(x[:,0,:].flatten(), label="Truth")
